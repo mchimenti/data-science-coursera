6f8296dbbaeb6b20ce830028595f98f962b07d28
chimenti <michael.chimenti@gmail.com>
2014-05-05 17:30:11 -0700

    add AUC function to roc_plot_gen

--- roc_plot_gen.R
+++ roc_plot_gen.R
@@ -3,7 +3,34 @@
 #first column is score (i.e, docking score) and second column is hit y/n (denoted as 1 or 0). 
 #Michael Chimenti, April 2014
 
+## check if required pracma package is present, if not, install it
+
+checkPkgs <- function() {
+        pkg.inst <- installed.packages()
+        pkgs <- ("pracma")
+        have.pkg <- pkgs %in% rownames(pkg.inst)
+        
+        if(any(!have.pkg)) {
+                cat("Some packages need to be installed\n")
+                r <- readline("Install necessary packages [y/n]? ")
+                if(tolower(r) == "y") {
+                        need <- pkgs[!have.pkg]
+                        message("installing packages ",
+                                paste(need, collapse = ", "))
+                        install.packages(need)
+                }
+        }
+}
+
+## call checkpg first
+
+checkPkgs()
+
+## roc_plot_gen
+
 roc_plot_gen <- function(file) {
+        
+        library(pracma)
         
         ## open the supplied file ##
         
@@ -20,15 +47,13 @@ roc_plot_gen <- function(file) {
         
         roc_sorted <- roc_table[order(roc_table[,1]),]
         
-        str(roc_sorted)
-        
         ## calculate number of false and true negatives ##
         
         true_neg <- nrow(subset(roc_sorted, roc_sorted[,2]==0))
-        print(true_neg)
+        cat("Number of true negatives = ", true_neg, "\n")
         
         false_neg <- nrow(subset(roc_sorted, roc_sorted[,2]==1))
-        print(false_neg)
+        cat("Number of false negatives = ", false_neg, "\n")
         
         ## create and initialize true positive and true negative vectors ##
         
@@ -71,16 +96,26 @@ roc_plot_gen <- function(file) {
        
         false_pos_rate <- lapply(false_pos_vec, function(false_pos_vec){false_pos_vec/true_neg})
 
-        ## calculate AUC ##
+        ## calculate AUC using "trapz" function from library "pracma" ##
         
+        auc <- trapz(as.numeric(false_pos_rate), as.numeric(true_pos_rate))
         
+        ## force rounding to 2 digits
         
-        ## plot True Pos rate as function of False Pos rate ##
+        auc_r <- round(auc, digits=2)
         
-        plot(y= true_pos_rate, x= false_pos_rate, type='l', xlab='False positive rate', ylab='True positive rate', main='ROC Analysis', cex.lab=1.65, cex.axis=1.5, lwd=5, xaxs='i', yaxs='i', ylim=c(0,1), mgp=c(2.5,1,0))
+        ## output AUC value
+        
+        cat("AUC is equal to:", auc_r)
+        
+        ## Do the ROC PLOT; True Pos rate as function of False Pos rate ##
+        
+        plot(y= true_pos_rate, x= false_pos_rate, type='l', xlab='False positive rate', ylab='True positive rate', main=paste('ROC Analysis; AUC =', auc_r), cex.lab=1.65, cex.axis=1.5, lwd=5, xaxs='i', yaxs='i', ylim=c(0,1), mgp=c(2.5,1,0))
         
         abline(0,1)
        
         
         
 }
+
+
